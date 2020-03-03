0ef77f46d59a82f4dc24a15ab80bf7303d6a5465
liam roberts <liamroberts77@googlemail.com>
2020-03-03 16:47:48 +0000

    using post instead of get

--- app.rb
+++ app.rb
@@ -11,8 +11,25 @@ get '/' do
   get '/food' do
     "Chips"
   end
+  get '/named-cat' do
+    p params
+    # @name = params[:name]
+    # @age = params[:age]
+    # erb(:index)
+  end
 
-  get '/cat' do
-    @rand_name = ["amigo","mendoza","william"].sample
-   erb(:index)
+  get '/cat_form' do
+    erb :cat_form
+  end
+
+  post '/cat-naming' do
+    @name = params[:name]
+    erb :index
+  end
+
+  get '/random-cat' do
+    @rand_name = ["amigo","mendoza","eduardo"].sample
+    @colour = params[:colour]
+    @rand_age = ["10", "5", "50", "99"].sample
+  #  erb(:index)
     end
